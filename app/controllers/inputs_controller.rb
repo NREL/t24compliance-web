f55bda02368c72030afda12306d576e8fea47596
kflemin <katherine.fleming@nrel.gov>
2014-11-10 08:56:56 -0700

    adding nrel fields to drive data forms

--- app/controllers/inputs_controller.rb
+++ app/controllers/inputs_controller.rb
@@ -12,12 +12,46 @@ class InputsController < ApplicationController
 	end
 
 	def show
-		@input = Structure.find(params[:id])
+		@input = Input.find(params[:id])
 		respond_to do |format|
 	    format.json do
 	      render :json => @input.as_json
 	    end
 	    format.html
 	  end
+	end
+
+	def datafields
+	
+		@input = Input.find(params[:input_id])
+
+		# save changes
+		if request.post?
+
+			@input.data_fields.each do |df|
+				# save added fields
+				if params[:exposed_fields] and params[:exposed_fields].include? df['name']
+					df['exposed'] = true
+				else
+					df['exposed'] = false 
+				end
+
+				if params[:set_as_constant_fields] and params[:set_as_constant_fields].include? df['name']
+					df['set_as_constant'] = true
+				else
+					df['set_as_constant'] = false
+				end
+
+				df['constant_value'] =  params[df['name'] + '_constant']
+
+			end
+			@input.save!
+		end
+
+
+		respond_to do |format|
+	    format.html
+	  end
+
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
-  resources :inputs, :only => [:index] 
+  resources :inputs, :only => [:index] do
+    get 'datafields'
+    match 'datafields' => 'inputs#datafields', :via => [:get, :post]
+  end
 
   root :to => "inputs#index"
 end
