e44944e66dfdd29dfdf04d0b8e83bbb98a1030e2
Nicholas Long <nicholas.long@nrel.gov>
2014-11-13 15:02:12 -0700

    start at parsing results

--- app/models/simulation.rb
+++ app/models/simulation.rb
@@ -4,24 +4,29 @@ class Simulation
 
   field :filename, type: String
   field :status, type: String
+  field :cbecc_code, type: Integer
+  field :cbecc_code_description, type: String
 
+  # compliance report
+  field :compliance_report_pdf_path, type: String
 
+  #embeds_many :simulation_results
 
   # Run the data point. The question I have here is whether or not this can be accessed from delayed_job
   def run_docker
-
     run_dir = File.join(Rails.root, 'data', 'simulations', self._id)
     FileUtils.mkdir_p run_dir
 
     # For now just copy in the example model that we are running into the folder under the new filename
     test_file = File.join(Rails.root, 'test/integration/files/cbecc_com_files/0200016-OffSml-SG-BaseRun.xml')
     run_file = File.join(run_dir, File.basename(test_file))
+    run_path = File.dirname(run_file)
+    run_filename = File.basename(run_file)
 
     if File.exists? run_file
-      fail "File #{run_file} already exists. Cannot run the analysis."
+      #fail "File #{run_file} already exists. Cannot run the analysis."
     end
     FileUtils.copy test_file, run_file
-
 
     # TODO: Move this to a delayed job task
     require 'docker'
@@ -42,7 +47,6 @@ class Simulation
         ssl_ca_file: File.join(cert_path, 'ca.pem'),
         scheme: 'https' # This is important when the URL starts with tcp://
     }
-    #
     Docker.url = ENV['DOCKER_HOST']
 
     # What is the longest timeout?
@@ -50,31 +54,65 @@ class Simulation
     Excon.defaults[:write_timeout] = docker_container_timeout
     Excon.defaults[:read_timeout] = docker_container_timeout
 
-    # Example file to run.
-    current_dir = Dir.pwd
-    begin
-      fail "Simulation file does not exist: #{run_file}" unless File.exist? run_file
+    # # Example file to run.
+    # current_dir = Dir.pwd
+    # begin
+    #   fail "Simulation file does not exist: #{run_file}" unless File.exist? run_file
+    #
+    #   Dir.chdir(run_path)
+    #   puts "Current working directory is: #{Dir.getwd}"
+    #
+    #   run_command = %W[/var/cbecc-com-files/run.sh -i /var/cbecc-com-files/run/#{run_filename}]
+    #   c = Docker::Container.create('Cmd' => run_command,
+    #                                'Image' => 'nllong/cbecc-com:daemon',
+    #                                'AttachStdout' => true,
+    #   )
+    #   c.start('Binds' => ["#{run_path}:/var/cbecc-com-files/run/"])
+    #
+    #   # this command is kind of weird. From what I understand, this is the container timeout (defaults to 60 seconds)
+    #   # This may be of interest: http://kimh.github.io/blog/en/docker/running-docker-containers-asynchronously-with-celluloid/
+    #   c.wait(docker_container_timeout)
+    #   stdout, stderr = c.attach(:stream => false, :stdout => true, :stderr => true, :logs => true)
+    #
+    #   logger.debug stdout
+    #   logger.info "Finished running simulation"
+    # ensure
+    #   Dir.chdir current_dir
+    # end
 
-      Dir.chdir(File.dirname(run_file))
-      puts "Current working directory is: #{Dir.getwd}"
+    # Post process the simulation
 
-      run_command = %W[/var/cbecc-com-files/run.sh -i /var/cbecc-com-files/run/#{File.basename(run_file)}]
-      #run_command = %w[find . -name *]
-      c = Docker::Container.create('Cmd' => run_command,
-                                   'Image' => 'nllong/cbecc-com:daemon',
-                                   'AttachStdout' => true,
-      )
+    # Clean up some of the files that are not needed
 
-      c.start('Binds' => ["#{File.dirname(run_file)}:/var/cbecc-com-files/run/"])
-
-      # this command is kind of weird. From what I understand, this is the container timeout (defaults to 60 seconds)
-      # This may be of interest: http://kimh.github.io/blog/en/docker/running-docker-containers-asynchronously-with-celluloid/
-      c.wait(docker_container_timeout)
-      stdout, stderr = c.attach(:stream => false, :stdout => true, :stderr => true, :logs => true)
-
-      logger.debug stdout
-      logger.info "Finished running simulation"
+    %w(runmanager.db).each do |f|
+      logger.debug "removing file: #{run_path}/#{f}"
+      File.delete File.join(run_path, f) if File.exist? File.join(run_path, f)
     end
+
+    # Save the state based on the CbeccComWrapper.json file that is persisted
+    result_file = File.join(run_path, "CbeccComWrapper.json")
+
+    Dir["#{run_path}/*"].each do |f|
+      if f =~ /AnalysisResults-BEES.pdf/
+        logger.info "saving the compliance report path to model"
+        self.compliance_report_pdf_path = f
+      elsif f =~ /CbeccComWrapper.json/
+        j = MultiJson.load(File.read(f), symbolize_keys: true) if File.exist?(f)
+        logger.info j
+
+        self.cbecc_code = j.keys.first
+        self.cbecc_code_description = j.values.first
+      elsif f =~ /.*\s-\sab.*/
+        logger.info "AB results"
+
+      end
+
+
+    end
+
+    self.save!
+
+
   end
 
 end
