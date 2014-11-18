44738b8a89874db4329ca120bf402be476446acc
kflemin <katherine.fleming@nrel.gov>
2014-11-18 15:43:34 -0700

    user rails4 private methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   # GET /users
   # GET /users.json
@@ -10,7 +11,7 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
-   @user = User.find(params[:id])
+
   end
 
   # GET /users/new
@@ -20,8 +21,6 @@ class UsersController < ApplicationController
 
   # GET /users/1/edit
   def edit
-
-    @user = User.find(params[:id])
 
   end
 
@@ -70,6 +69,18 @@ class UsersController < ApplicationController
   def admin
 
   end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def user_params
+    params.require(:user).permit(:email, :roles => [])
+  end
+
 
   
 end
