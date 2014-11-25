9db2f9f359e0a4fabbdf1860c378bdf7ad3f4a37
Alex Swindler <Alex.Swindler@nrel.gov>
2014-11-25 10:20:14 -0700

    Updated Angular to v1.3.4 Added lodash Removed underscore

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,6 +18,6 @@
 //= require angular-rails-templates
 //= require angular-resource/angular-resource
 //= require angular-flash/dist/angular-flash
-//= require underscore
+//= require lodash
 //= require_tree .
 
