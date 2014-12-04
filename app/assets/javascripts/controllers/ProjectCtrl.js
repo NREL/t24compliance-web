c70ae05389f04d253245945003a200da5fe4f249
kflemin <katherine.fleming@nrel.gov>
2014-12-04 10:33:51 -0700

    project template

--- app/assets/javascripts/controllers/ProjectCtrl.js
+++ app/assets/javascripts/controllers/ProjectCtrl.js
@@ -3,7 +3,48 @@ var ctrls;
 ctrls = angular.module('controllers');
 
 ctrls.controller("ProjectCtrl", [
-  '$scope', '$routeParams', '$resource', '$location', 'flash', function($scope, $routeParams, $resource, $location, flash) {
+  '$scope', '$window', '$routeParams', '$resource', '$location', 'flash', 'Project', function($scope, $window, $routeParams, $resource, $location, flash, Project) {
 
-  }
+  	// pull in global enum definitions
+  	$scope.project_compliance_type_enums = $window.project_compliance_type_enums;
+
+		if ($routeParams.id) {
+		    $scope.project = Project.show({ id: $routeParams.id });
+		} 
+		else {
+		    $scope.project = new Project();
+		}
+	  
+	  $scope.submit = function() {
+	    console.log("submit")
+
+	    function success(response) {
+	      console.log("success", response)
+	      $location.path("/projects/"+$scope.project.id);
+	    }
+
+	    function failure(response) {
+	      console.log("failure", response)
+
+	      _.each(response.data, function(errors, key) {
+	        _.each(errors, function(e) {
+	          $scope.form[key].$dirty = true;
+	          $scope.form[key].$setValidity(e, false);
+	        });
+	      });
+	    }
+
+	    if ($routeParams.id) {
+	      Project.update($scope.project, success, failure);
+	    } else {
+	      Project.create($scope.project, success, failure);
+	    }
+
+	  };
+
+	  $scope.cancel = function() {
+    	$location.path("/projects/"+$scope.project.id);
+  	};
+
+	}
 ]);
---
+++ app/assets/javascripts/services/Project.js
@@ -0,0 +1,11 @@
+app.factory("Project", function($resource) {
+  return $resource("/projects/:id", { id: "@id" },
+    {
+      'create':  { method: 'POST' },
+      'index':   { method: 'GET', isArray: true },
+      'show':    { method: 'GET', isArray: false },
+      'update':  { method: 'PUT' },
+      'destroy': { method: 'DELETE' }
+    }
+  );
+});
