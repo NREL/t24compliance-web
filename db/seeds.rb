5ca425277d75eae1375b53f9b1388bacc960aab9
Nicholas Long <nicholas.long@nrel.gov>
2014-12-05 11:28:21 -0700

    move construction loading to rake db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+u = User.find_or_create_by(email: 'test@nrel.gov')
+u.roles = [:admin]
+
+# Remove this if we share the code. This is very dangerous giving the admin a simple password for testing sake!
+u.password = 'password'
+u.save!
+
+#User.project.delete_all
+
+file = File.read(File.join(Rails.root,"lib/assets/construction_library.json"))
+data = JSON.parse(file)
+
+data['constructions'].each_with_index do |c, index|
+
+  cons = Construction.find_or_create_by(name: c['name'])
+  if cons
+    puts "Adding/Updating Construction: #{cons.name}"
+
+    # Note that this is a one-way merge. It will not remove any fields that are in the current record instance that
+    # need to be removed
+    c = cons.as_json.merge(c)
+    cons.update(c)
+    cons.save!
+  end
+  # break if index == 10
+end
