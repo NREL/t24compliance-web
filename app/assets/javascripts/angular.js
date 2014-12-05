44ca59c6c10b045708dcebcba1c2699d058d0d42
Nicholas Long <nicholas.long@nrel.gov>
2014-12-05 10:20:31 -0700

    add javascript libraries

--- app/assets/javascripts/angular.js
+++ app/assets/javascripts/angular.js
@@ -1,10 +1,16 @@
 // Manifest for angular app
 
 //= require angular/angular
-//= require angular-route/angular-route
 //= require angular-rails-templates
-//= require angular-resource/angular-resource
+//= require angular-animate/angular-animate
 //= require angular-flash/dist/angular-flash
+//= require angular-mocks/angular-mocks
+//= require angular-resource/angular-resource
+//= require angular-route/angular-route
+//= require angular-strap/dist/angular-strap
+//= require angular-ui-grid/ui-grid
+//= require angular-ui-router/release/angular-ui-router
+//= require bootstrap/dist/js/bootstrap
 //= require lodash
 //= require angular/cbecc
 //= require_tree  ./angular/templates
