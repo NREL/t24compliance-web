dd12b4af4e38f969d41588ff9c2aa592e350edd6
Alex Swindler <Alex.Swindler@nrel.gov>
2014-12-05 14:41:46 -0700

    Adding Constructions UI-Grid Pipelining UI-Grid fonts Fixing Bootstrap errors

--- app/assets/javascripts/angular/controllers/ConstructionsCtrl.js
+++ app/assets/javascripts/angular/controllers/ConstructionsCtrl.js
@@ -1,7 +1,47 @@
 cbecc.controller('ConstructionsCtrl', [
   '$scope', '$window', '$routeParams', '$resource', '$location', 'flash', function ($scope, $window, $routeParams, $resource, $location, flash) {
 
-
+    $scope.data = [
+      {
+        "Category": "Concrete",
+        "Standard Material Selection": "Concrete - 140 lb/ft3 -4 in",
+        "R-Value": 0.3,
+        "Thickness": 4.0,
+        "Specific Heat": 0.22,
+        "Density": 139.78,
+        "Thermal Conductivity": 1.128,
+        "Cavity Insulation R-Value": null,
+        "Framing": "",
+        "Configuration": "",
+        "Depth": null
+      },
+      {
+        "Category": "Composite",
+        "Standard Material Selection": "",
+        "R-Value": 10.0,
+        "Thickness": 4.0,
+        "Specific Heat": 0.22,
+        "Density": 139.78,
+        "Thermal Conductivity": null,
+        "Cavity Insulation R-Value": 0.5,
+        "Framing": "Wood",
+        "Configuration": "Wall 24 inch OC",
+        "Depth": 2
+      },
+      {
+        "Category": "Wood",
+        "Standard Material Selection": "Hardwood 1/2 inch",
+        "R-Value": 0.43,
+        "Thickness": 0.5,
+        "Specific Heat": 0.39,
+        "Density": 42.43,
+        "Thermal Conductivity": .097,
+        "Cavity Insulation R-Value": null,
+        "Framing": "",
+        "Configuration": "",
+        "Depth": null
+      }
+    ];
 
   }
 ]);
--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,7 @@ module CbeccComWeb
     # config.i18n.default_locale = :de
 
     config.assets.paths << Rails.root.join("vendor","assets","bower_components")
+    config.assets.paths << Rails.root.join("vendor","assets","bower_components","angular-ui-grid")
     config.assets.paths << Rails.root.join("vendor","assets","bower_components","bootstrap-sass-official","assets","fonts")
 
     config.assets.precompile << %r(.*.(?:eot|svg|ttf|woff)$)
