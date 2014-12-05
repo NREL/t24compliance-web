09029a10127deb59e2919bbb1084260c572873fa
kflemin <katherine.fleming@nrel.gov>
2014-12-05 10:13:13 -0700

    adding constructions lib

---
+++ app/models/construction.rb
@@ -0,0 +1,15 @@
+class Construction
+  include Mongoid::Document
+	include Mongoid::Timestamps
+	field :name, type: String
+	field :compatible_surface_type, type: String
+	field :type, type: String
+	field :framing_configuration, type: String
+	field :framing_size, type: String
+	field :cavity_insulation_r_value, type: Float
+	field :continuous_insulation_r_value, type: Float
+	field :continuous_insulation_material_name, type: String
+	field :layers, type: Array
+
+
+end
