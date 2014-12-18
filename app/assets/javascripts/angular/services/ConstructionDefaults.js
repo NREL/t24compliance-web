a84a7ca3b27eb2d9bf217a671e5fb37ae787b258
Petersen, Anya <Anya.Petersen@nrel.gov>
2014-12-17 18:40:11 -0700

    Remove unused actions

--- app/assets/javascripts/angular/services/ConstructionDefaults.js
+++ app/assets/javascripts/angular/services/ConstructionDefaults.js
@@ -1,11 +1,7 @@
 cbecc.factory('ConstructionDefaults', ['$resource',function($resource) {
   return $resource('construction_defaults/:id.json', { project_id: '@projectId', id: '@id' },
     {
-      'create':  { method: 'POST' },
       'index':   { method: 'GET', isArray: true },
-      'show':    { method: 'GET', isArray: false },
-      'update':  { method: 'PUT' },
-      'destroy': { method: 'DELETE' }
     }
   );
 }]);
