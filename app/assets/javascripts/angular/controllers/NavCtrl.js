4649e1437d62bd5239fb577334638aace5f15bb7
Petersen, Anya <Anya.Petersen@nrel.gov>
2014-12-17 21:51:00 -0700

    Regex to set active tabs.

--- app/assets/javascripts/angular/controllers/NavCtrl.js
+++ app/assets/javascripts/angular/controllers/NavCtrl.js
@@ -1,14 +1,8 @@
 cbecc.controller("NavCtrl", [
   '$scope', '$location', 'Shared', function ($scope, $location, Shared) {
-    $scope.isActive = function(viewLocation) {
-      if (viewLocation == '/') {
-        if ($location.path() == '/') return true;
-        return false;
-      }
-      if ($location.path().substr(0, viewLocation.length) == viewLocation) {
-        return true;
-      }
-      return false;
+    $scope.isActive = function(tabIndicator) {
+      var tabRegex = new RegExp(tabIndicator);
+      return tabRegex.test($location.path());
     };
     $scope.projectPath = function() {
       return Shared.projectPath();
