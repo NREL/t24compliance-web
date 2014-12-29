b7eee39cc2d7d3688560034a77fb9f07cceb297a
Alex Swindler <Alex.Swindler@nrel.gov>
2014-12-29 13:28:57 -0600

    Fix project checkbox logic

--- app/assets/javascripts/angular/controllers/ProjectCtrl.js
+++ app/assets/javascripts/angular/controllers/ProjectCtrl.js
@@ -15,6 +15,12 @@ cbecc.controller('ProjectCtrl', [
     if (proj_id) {
       Project.show({id: proj_id}).$promise.then(function (response) {
         $scope.project = response;
+        // Exceptional_condition_modeling doesn't actually have a field, Initialize checkbox
+        if ($scope.project.exceptional_condition_narrative) {
+          $scope.project.exceptional_condition_modeling = 'Yes';
+        } else {
+          $scope.project.exceptional_condition_modeling = 'No';
+        }
       }, function (response) {
         if (response.status == 404) {
           Shared.setProjectId(null);
@@ -26,29 +32,35 @@ cbecc.controller('ProjectCtrl', [
       });
     } else {
       $scope.project = new Project();
+      // Initialize checkboxes
+      $scope.project.exceptional_condition_no_cooling_system = 'No';
+      $scope.project.exceptional_condition_rated_capacity = 'No';
+      $scope.project.exceptional_condition_water_heater = 'No';
+      $scope.project.exceptional_condition_modeling = 'No';
     }
 
     // save
     $scope.submit = function () {
       console.log("submit");
-      $scope.errors = {} ; //clean up server errors
+      $scope.errors = {}; //clean up server errors
 
-      console.log('EXCEPTIONALs');
+      console.log('EXCEPTIONALS');
       console.log('no cooling: ', $scope.project.exceptional_condition_no_cooling_system);
       console.log('rated capacity: ', $scope.project.exceptional_condition_rated_capacity);
       console.log('water heater: ', $scope.project.exceptional_condition_water_heater);
 
-      //TODO:  when saving, check if exceptional_condition_modeling is true.  If so, save exceptional_condition_narrative.
-      // Exceptional_condition_modeling doesn't actually have a field
-
+      // Exceptional_condition_modeling doesn't actually have a field, clear narrative if checkbox is 'No'
+      if ($scope.project.exceptional_condition_modeling == 'No') {
+        $scope.project.exceptional_condition_narrative = null;
+      }
 
       function success(response) {
         toaster.pop('success', 'Project successfully saved');
-        the_id = typeof response['id'] === "undefined" ? response['_id'] : response['id'];
+        the_id = response.hasOwnProperty('id') ? response.id : response._id;
 
         // go back to form with id of what was just saved
         Shared.setProjectId(the_id);
-        console.log("redirecting to "+Shared.projectPath());
+        console.log("redirecting to " + Shared.projectPath());
         $location.path(Shared.projectPath());
 
       }
@@ -57,7 +69,7 @@ cbecc.controller('ProjectCtrl', [
         console.log("failure", response);
         toaster.pop('error', 'An error occurred while saving');
 
-        return angular.forEach(response.data.errors, function(errors, field) {
+        return angular.forEach(response.data.errors, function (errors, field) {
           $scope.form[field].$setValidity('server', false);
           return $scope.errors[field] = errors.join(', ');
         });
@@ -72,11 +84,10 @@ cbecc.controller('ProjectCtrl', [
     };
 
     // Form Errors
-    $scope.errorClass = function(name) {
+    $scope.errorClass = function (name) {
       var s = $scope.form[name];
       return s.$invalid && s.$dirty ? "has-error" : "";
     };
-
 
 
   }
