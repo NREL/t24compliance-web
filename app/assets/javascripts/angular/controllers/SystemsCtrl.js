4109e756c021168d4f4e6d3d20395e17616a398f
kflemin <katherine.fleming@nrel.gov>
2014-12-30 21:41:38 -0700

    ptac fan

--- app/assets/javascripts/angular/controllers/SystemsCtrl.js
+++ app/assets/javascripts/angular/controllers/SystemsCtrl.js
@@ -56,8 +56,9 @@ cbecc.controller('SystemsCtrl', [
     initTabs();
     $scope.setActiveTab('ptac', 0);
 
-    //get systems by type
+    //TODO: get systems by type
     $scope.ptacs = [];
+    $scope.ptac_fans = [];
 
     // PTAC general UI Grid
     $scope.ptacGridOptions = {
@@ -91,6 +92,58 @@ cbecc.controller('SystemsCtrl', [
     };
     $scope.ptacGridOptions.data = $scope.ptacs;
 
+    // PTAC fan UI Grid
+    $scope.ptacFanGridOptions = {
+      columnDefs: [{
+        name: 'name',
+        displayName: 'System Name'
+      }, {
+        name: 'fan_name',
+        displayName: 'Fan Name'
+      },{
+        name: 'control_method',
+        displayName: 'Control Method'
+      }, {
+        name: 'flow_efficiency',
+        displayName: 'Flow Efficiency'
+      }, {
+        name: 'total_static_pressure',
+        displayName: 'Total Static Pressure'
+      }, {
+        name: 'motor_position',
+        displayName: 'Motor Position'
+      }, {
+        name: 'motor_hp',
+        displayName: 'Motor HP'
+      }, {
+        name: 'motor_type',
+        displayName: 'Motor Type'
+      }, {
+        name: 'motor_pole_count',
+        displayName: 'Motor Pole Count'
+      }, {
+        name: 'motor_efficiency',
+        displayName: 'Motor Efficiency'
+      }],
+      enableCellEditOnFocus: true,
+      enableColumnMenus: false,
+      enableRowHeaderSelection: true,
+      enableRowSelection: true,
+      enableSorting: false,
+      multiSelect: false,
+      onRegisterApi: function (gridApi) {
+        $scope.gridApi = gridApi;
+        gridApi.selection.on.rowSelectionChanged($scope, function (row) {
+          if (row.isSelected) {
+            $scope.selected = row.entity;
+          } else {
+            // No rows selected
+            $scope.selected = null;
+          }
+        });
+      }
+    };
+    $scope.ptacGridOptions.data = $scope.ptac_fans;
 
     //add functions
     $scope.addPTAC = function () {
