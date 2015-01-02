b5fe9e762ce77f5ee5d2833c4b3767476fe9b773
Alex Swindler <Alex.Swindler@nrel.gov>
2015-01-02 16:04:13 -0700

    Small cleanup

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
 require File.expand_path('../boot', __FILE__)
 
-#r equire 'rails/all'
+# require 'rails/all'
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "sprockets/railtie"
