c48c687581397530a77cd592420cc36a29ec11fa
Alex Swindler <Alex.Swindler@nrel.gov>
2015-01-11 23:27:59 -0700

    Added stub to update space defaults if space function changes

--- app/assets/javascripts/angular/controllers/SpacesSettingsCtrl.js
+++ app/assets/javascripts/angular/controllers/SpacesSettingsCtrl.js
@@ -73,6 +73,40 @@ cbecc.controller('SpacesSettingsCtrl', ['$scope', 'Shared', 'Enums', function ($
           $scope.selected.space = null;
         }
       });
+      gridApi.edit.on.afterCellEdit($scope, function (rowEntity, colDef, newValue, oldValue) {
+        // Update unmodified defaults
+        if (colDef.name == 'space_function' && newValue != oldValue) {
+          // TODO Lookup space defaults
+          if (rowEntity.occupant_density == rowEntity.occupant_density_default) {
+            rowEntity.occupant_density = 10;
+          }
+          if (rowEntity.hot_water_heating_rate == rowEntity.hot_water_heating_rate_default) {
+            rowEntity.hot_water_heating_rate = 0.18;
+          }
+          if (rowEntity.receptacle_power_density == rowEntity.receptacle_power_density_default) {
+            rowEntity.receptacle_power_density = 1.5;
+          }
+          if (rowEntity.exhaust_per_area == rowEntity.exhaust_per_area_default) {
+            rowEntity.exhaust_per_area = 10;
+          }
+          if (rowEntity.exhaust_air_changes_per_hour == rowEntity.exhaust_air_changes_per_hour_default) {
+            rowEntity.exhaust_air_changes_per_hour = 0.18;
+          }
+          if (rowEntity.exhaust_per_space == rowEntity.exhaust_per_space_default) {
+            rowEntity.exhaust_per_space = 250;
+          }
+
+          rowEntity.occupant_density_default = 10;
+          rowEntity.hot_water_heating_rate_default = 0.18;
+          rowEntity.receptacle_power_density_default = 1.5;
+
+          rowEntity.exhaust_per_area_default = 10;
+          rowEntity.exhaust_air_changes_per_hour_default = 0.18;
+          rowEntity.exhaust_per_space_default = 250;
+
+          gridApi.core.notifyDataChange(gridApi.grid, uiGridConstants.dataChange.EDIT);
+        }
+      });
     }
   };
 }]);
