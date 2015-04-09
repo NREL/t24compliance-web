0180e1643f3e4d69f8956ee5eaf9deaf49872bf6
kflemin <katherine.fleming@nrel.gov>
2015-04-08 20:21:22 -0600

    compliance tab enhancements

--- app/assets/javascripts/angular/controllers/ComplianceCtrl.js
+++ app/assets/javascripts/angular/controllers/ComplianceCtrl.js
@@ -1,14 +1,24 @@
-cbecc.controller('ComplianceCtrl', ['$scope', '$log', 'data', 'Shared', 'simulations', function ($scope, $log, data, Shared, simulations) {
+cbecc.controller('ComplianceCtrl', ['$scope', '$log', '$timeout','data', 'Shared', 'simulations', function ($scope, $log, $timeout, data, Shared, simulations) {
 
   $scope.simulation = simulations[0];
-  console.log('HI!');
   console.log($scope.simulation);
 
+  $scope.errors_open = false;
+
   $scope.isComplete = function() {
-    return $scope.simulation['status'] == 'completed'
+    return $scope.simulation.status  == 'completed' || $scope.simulation.status == 'errored'
+  };
+  $scope.isRunning = function() {
+    return (($scope.simulation.status == 'init') || ($scope.simulation['status'] == 'queued') || ($scope.simulation['status'] == 'started'))
   };
 
+  $scope.hasErrors = function() {
+    return $scope.simulation.error_messages.length > 0
+  };
 
+  $scope.hasCompliancePDF = function()  {
+    return $scope.simulation['compliance_report_pdf_path'] == null;
+  };
   $scope.getXml = function () {
     $log.debug('Submitting simulation to run queue');
 
